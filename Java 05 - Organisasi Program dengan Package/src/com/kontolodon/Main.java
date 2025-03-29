dda20a746a508cbc6a509a6ff0296d915c2b1b53
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-03-30 01:06:04 +0700

    first commit

---
+++ Java 05 - Organisasi Program dengan Package/src/com/kontolodon/Main.java
@@ -0,0 +1,7 @@
+package src.com.kontolodon;
+
+public class Main {
+    public static void main(String [] args){
+        System.out.println("私はAnas");
+    }
+}
---
+++ Java 06 - Pengenalan Print dan Alur Eksekusi/src/com/tutorial/Main.java
@@ -0,0 +1,21 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String [] args){
+        //print dengan langsung enter
+        System.out.println("Hello Sekai!!");
+        System.out.println("Hallo jancok 1!!");
+
+        //print tanpa enter langsung
+        System.out.print("Hallo jancok 2!!");
+        System.out.print("Hallo jancok xx!!");
+
+        //print dengan enter manual
+        System.out.print("Hallo jancok 3!! \n");
+        System.out.print("Hallo jancok 4!! \n");
+
+        //print dengan format dengan angka dll
+        System.out.printf("Hallo kimax", 1994);
+        System.out.printf("Hallo kimax ku", 1773);
+    }
+}
