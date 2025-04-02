cc66a6b1ad0e31ebe7d3c263595fb2c5e34c4668
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-04-02 23:39:45 +0700

    new learn update

---
+++ Java 07 - Memahami String args/src/com/tutorial/Main.java
@@ -0,0 +1,8 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello " + args[0] + " Jancok");
+        System.out.println("Hello " + args[1] + " Rek");
+    }
+}
---
+++ Java 08 - Variabel Assignment dan Deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,18 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[]args){
+        // buat variable
+        int x = 10; //assingment
+        System.out.println("ini nilai x = " + x);
+
+        //deklarasi ulang
+        x = 1998;
+        System.out.println("ini nilai x ke 2 = " + x);
+
+        //buat deklarasi
+        int y; //deklarasi
+        y = 69;
+        System.out.println("ini nilai y ke 2 = " + y);
+    }
+}
